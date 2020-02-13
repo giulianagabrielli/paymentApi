7825508bf554bd9f1d1305a7b307c174d89f4fe7
giulianagabrielli <glngabrielli@gmail.com>
2020-02-13 14:48:53 -0300

    falta if para findPayment se o pagamento for boleto ou cartão

--- payments/findPayment.php
+++ payments/findPayment.php
@@ -15,53 +15,80 @@
     $database = new Database();
     $connection = $database->connect();
 
-    //new Boleto Payment
-    $transactionBoleto = new Transaction();
+    //new Payment
+    $transaction = new Transaction();
 
-    $transactionBoleto->clientId = isset($_GET['clientId']) ? $_GET['clientId'] : die();
+    $transaction->clientId = isset($_GET['clientId']) ? $_GET['clientId'] : die();
+
+    if($transaction->creditCard['cardHolderName'] == null){
+        //boleto
+        $transaction->findBoletoPayment(); 
+        
+        if($transaction){  
     
-    //boleto
-    $transactionBoleto->findBoletoPayment(); 
+            $transaction_arr = array(
+                'clientId' => $transaction->clientId,
+                'buyer'=> [
+                    'name' => $transaction->buyer['name'], 
+                    'email' => $transaction->buyer['email'], 
+                    'cpf' => $transaction->buyer['cpf']
+                ],
+                'payment' => [
+                    'type' => $transaction->payment['type'], 
+                    'amount' => $transaction->payment['amount']
+                ],
+                'boletoNumber' => "1234 4556 6778 8888888"
+            );
     
-    if($transactionBoleto){  
+            //turn to json
+            echo json_encode($transaction_arr);
+    
+        } else { 
+            echo json_encode(
+                array('message'=> 'No payment found')
+            );
+        }
 
-        $transaction_arr = array(
-            'clientId' => $transactionBoleto->clientId,
-            'buyer'=> ['name' => $transactionBoleto->buyer['name'], 'email' => $transactionBoleto->buyer['email'], 'cpf' => $transactionBoleto->buyer['cpf']],
-            'payment' => ['type' => $transactionBoleto->payment['type'], 'amount' => $transactionBoleto->payment['amount']],
-            'boletoNumber' => "1234 4556 6778 8888888"
-        );
 
-        //turn to json
-        echo json_encode($transaction_arr);
 
-    } else { 
-        echo json_encode(
-            array('message'=> 'No payment found')
-        );
+    } else {
+        //new Credit Card Payment
+        $transaction = new Transaction();
+    
+        // $transaction->clientId = isset($_GET['clientId']) ? $_GET['clientId'] : die();
+        $transaction->findCreditCardPayment(); 
+        
+        if($transaction){  
+    
+            $transaction_arr = array(
+                'clientId' => $transaction->clientId,
+                'buyer'=> [
+                    'name' => $transaction->buyer['name'], 
+                    'email' => $transaction->buyer['email'], 
+                    'cpf' => $transaction->buyer['cpf']
+                ],
+                'payment' => [
+                    'type' => $transaction->payment['type'], 
+                    'amount' => $transaction->payment['amount']
+                ],
+                'creditCard' => [
+                    'cardHolderName' => $transaction->creditCard['cardHolderName'], 'cardNumber' => $transaction->creditCard['cardNumber'], 
+                    'cardExpDate' => $transaction->creditCard['cardExpDate'], 
+                    'cardCvv' => $transaction->creditCard['cardCvv']
+                ],
+                'transaction' => "Transaction Successful!"
+            );
+    
+            //turn to json
+            echo json_encode($transaction_arr);
+    
+        } else { 
+            echo json_encode(
+                array('message'=> 'No payment found')
+            );
+        }
+
+
     }
-
-    //new Credit Card Payment
-    $transactionCredit = new Transaction();
-
-    $transactionCredit->clientId = isset($_GET['clientId']) ? $_GET['clientId'] : die();
-    $transactionCredit->findCreditCardPayment(); 
     
-    if($transactionCredit){  
 
-        $transaction_arr = array(
-            'clientId' => $transactionCredit->clientId,
-            'buyer'=> ['name' => $transactionCredit->buyer['name'], 'email' => $transactionCredit->buyer['email'], 'cpf' => $transactionCredit->buyer['cpf']],
-            'payment' => ['type' => $transactionCredit->payment['type'], 'amount' => $transactionCredit->payment['amount']],
-            'creditCard' => ['cardHolderName' => $transactionCredit->creditCard['cardHolderName'], 'cardNumber' => $transactionCredit->creditCard['cardNumber'], 'cardExpDate' => $transactionCredit->creditCard['cardExpDate'], 'cardCvv' => $transactionCredit->creditCard['cardCvv']],
-            'transaction' => "Transaction Successful!"
-        );
-
-        //turn to json
-        echo json_encode($transaction_arr);
-
-    } else { 
-        echo json_encode(
-            array('message'=> 'No payment found')
-        );
-    }
